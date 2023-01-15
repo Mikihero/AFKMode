6c5df71b8f45937ac1929721b1e504379ef0ab50
Mikihero <mikolajmelaniuk@gmail.com>
2023-01-15 01:10:34 +0100

    initial commit

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace AFKMode
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin should be enabled.")]
+        public bool IsEnabled { get; set; } = true;
+        
+        [Description("Whether or not debug logs should be shown.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Whether or not alive players will be able to use the command, remember that if an scp uses this there will be no communicate about it's death. Default: false")] 
+        public bool AllowUseWhileAlive { get; set; } = false;
+    }
+}
---
+++ commands/Overwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using PlayerRoles;
+using PluginAPI.Events;
+
+namespace AFKMode.commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class Overwatch : ICommand
+    {
+        public string Command => "overwatch";
+        public string[] Aliases => new[] { "ow", "owatch" };
+        public string Description => "Forceclasses you to overwatch.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player pl = Player.Get(sender);
+
+            if (!pl.CheckPermission("am.ow"))
+            {
+                response = "Invalid permissions.";
+                return false;
+            }
+
+            if (Round.IsLobby)
+            {
+                response = "You can't use this right now.";
+                return false;
+            }
+            
+            if (!Plugin.Instance.Config.AllowUseWhileAlive && pl.IsAlive)
+            {
+                response = "You can't use this while alive.";
+                return false;
+            }
+            if (pl.Role.Type == RoleTypeId.Overwatch)
+            {
+                Log.Debug($"Player: {pl.Nickname}({pl.RawUserId}) has turned off overwatch.");
+                pl.Role.Set(RoleTypeId.Spectator);
+                response = "Successfully turned off overwatch.";
+                return true;
+            }
+            Log.Debug($"Player: {pl.Nickname}({pl.RawUserId}) has turned on overwatch. They were: {pl.Role.Name}");
+            pl.Role.Set(RoleTypeId.Overwatch);
+            response = "Successfully turned on overwatch.";
+            return true;
+        }
+    }
+}
